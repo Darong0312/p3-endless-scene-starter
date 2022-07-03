46a29bebaadc1246fcb8488ed88ddcf49bbeda98
Darong Li <dli97@ucsc.edu>
2022-07-02 22:22:54 -0700

    added boat, ocean, island, flag

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # p3-endless-scene-starter
 
+# What is the effect you're trying to achieve?
+
+I am trying to have a plane that lies on the ocean. I imported the ocean tile, the island tile and boat tile.
+Island tile will have a slightly light blue on the background. I have this feature because when the ocean closer to the land, the color of the blue will be lighter.
+Also, when the user click of the tile, they will have a flag indicate on the tile. This means the user explore this area.
+
+I think I have Archieve the effect.
+
 Credit: Adam Smith/Isaac Karth 
-Edited by: Asiiah Song
+Edited by: Darong Li
--- world.js
+++ world.js
@@ -1,18 +1,11 @@
 "use strict";
+const grassColor = "#AFE1AF";
+const boatColor = "#C19A6C";
+const flagColor = "#FF0000";
+const oceanColor = "#c4f4fc";
+const rockColor = "#3a3232";
 
 /* global XXH */
-/* exported --
-    p3_preload
-    p3_setup
-    p3_worldKeyChanged
-    p3_tileWidth
-    p3_tileHeight
-    p3_tileClicked
-    p3_drawBefore
-    p3_drawTile
-    p3_drawSelectedTile
-    p3_drawAfter
-*/
 
 function p3_preload() {}
 
@@ -27,10 +20,10 @@ function p3_worldKeyChanged(key) {
 }
 
 function p3_tileWidth() {
-  return 16;
+  return 32;
 }
 function p3_tileHeight() {
-  return 16;
+  return 32;
 }
 
 let [tw, th] = [p3_tileWidth(), p3_tileHeight()];
@@ -47,21 +40,26 @@ function p3_drawBefore() {}
 
 function p3_drawTile(i, j) {
   noStroke();
+  
   fill(noise(i, j) * 255)
 
   push();
 
-  beginShape();
-  vertex(0, 0);
-  vertex(0, tw);
-  vertex(th, tw);
-  vertex(th, 0);
-  endShape(CLOSE);
+  drawFlag();
 
+  let random = noise(i,j);
+  if(random <= 0.2){
+    drawIsland();
+  }
+  else if(random <= 0.4 && random >=0.3){
+    drawBoat();
+  }
+  else{
+    drawOcean();
+  }
   let n = clicks[[i, j]] | 0;
   if (n % 2 == 1) {
-    fill(255, 255, 0, 180);
-    ellipse(th/2, tw/2, 10, 10);
+    drawFlag();
   }
 
   pop();
@@ -79,8 +77,69 @@ function p3_drawSelectedTile(i, j) {
   endShape(CLOSE);
 
   noStroke();
-  fill(0);
   text("(" + [i, j] + ")", 0, 0);
+}
+
+function drawFlag(){
+
+  beginShape();
+  fill(flagColor);
+  vertex(th/3*2,tw/3);
+  vertex(th/3*2,tw/8);
+  vertex(th/4,tw/5);
+  stroke(20);
+  endShape(CLOSE);
+
+  line(th/3*2,tw/3, th/3*2, tw/2);
+
+  stroke(127);
+
+}
+
+function drawOcean(){
+  fill("#0099FF");
+  noStroke();
+  rect(0, 0, th, tw);
+}
+
+function drawBoat(){
+  fill("#0099FF");
+
+  noStroke();
+  rect(0,0,th,tw);
+
+  // boat
+  fill(boatColor);
+  beginShape();
+  vertex(th/20, tw/2);
+  vertex(th, tw/2);
+  vertex(th/10*7, tw/7*5);
+  vertex(th/10*3, tw/7*5);
+  stroke(20);
+  endShape(CLOSE);
+  stroke(127);
+}
+
+function drawIsland(){
+
+  fill("#3399FF");
+  noStroke();
+  rect(0, 0, th, tw);
+
+  //rock
+  fill(rockColor);
+  beginShape();
+  vertex(th/7, tw/3);
+  vertex(th/7*3, tw/6);
+  vertex(th/7*4, tw/6);
+  vertex(th/7*5, tw/4);
+  vertex(th/9*8, tw/7*5);
+  vertex(th/11, tw/7*5);
+  stroke(20);
+  endShape(CLOSE);
+
+  stroke(127);
+
 }
 
 function p3_drawAfter() {}
